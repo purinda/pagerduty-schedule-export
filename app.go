b77200a88e5c44d521a152684d5fc4723469bf5b
Purinda Gunasekara <purinda@gmail.com>
2023-08-09 20:32:42 +1000

    Timezone parser implemented

--- app.go
+++ app.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strings"
+	"time"
 )
 
 const (
@@ -13,12 +14,12 @@ const (
 )
 
 type Event struct {
-	DTStart  string
-	DTEnd    string
+	DTStart  time.Time
+	DTEnd    time.Time
 	Attendee string
 }
 
-func ParseICal(data string) ([]Event, error) {
+func ParseICal(data string, location *time.Location) ([]Event, error) {
 	lines := strings.Split(data, "\n")
 	var events []Event
 	var currentEvent Event
@@ -28,9 +29,17 @@ func ParseICal(data string) ([]Event, error) {
 		case strings.HasPrefix(line, "BEGIN:VEVENT"):
 			currentEvent = Event{}
 		case strings.HasPrefix(line, "DTSTART"):
-			currentEvent.DTStart = strings.Split(line, ":")[1]
+			t, err := time.Parse(time.RFC3339, strings.Split(line, ":")[1])
+			if err != nil {
+				return nil, err
+			}
+			currentEvent.DTStart = t.In(location)
 		case strings.HasPrefix(line, "DTEND"):
-			currentEvent.DTEnd = strings.Split(line, ":")[1]
+			t, err := time.Parse(time.RFC3339, strings.Split(line, ":")[1])
+			if err != nil {
+				return nil, err
+			}
+			currentEvent.DTEnd = t.In(location)
 		case strings.HasPrefix(line, "ATTENDEE"):
 			currentEvent.Attendee = strings.Split(line, ":")[1]
 		case strings.HasPrefix(line, "END:VEVENT"):
@@ -66,16 +75,23 @@ func FetchData(url string) (string, error) {
 }
 
 func main() {
-	url := flag.String("url", "https://example-domain.com/pd-sched.ical", "iCal URL")
+	url := flag.String("url", "https://example.com/path-to-ical-file.ics", "URL to fetch iCal data")
+	tz := flag.String("timezone", "UTC", "Target timezone for date conversions (e.g., 'Australia/Sydney' for AEST)")
 	flag.Parse()
 
-	data, err := FetchData(*url)
+	location, err := time.LoadLocation(*tz)
 	if err != nil {
-		fmt.Printf("Error: %v\n", err)
+		fmt.Printf("Error loading timezone: %v\n", err)
 		return
 	}
 
-	events, err := ParseICal(data)
+	data, err := FetchData(*url)
+	if err != nil {
+		fmt.Printf("Error fetching iCal: %v\n", err)
+		return
+	}
+
+	events, err := ParseICal(data, location)
 	if err != nil {
 		fmt.Printf("Error parsing iCal data: %v\n", err)
 		return
